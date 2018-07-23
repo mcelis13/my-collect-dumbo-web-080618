e25a65f9f1c6e30338d39b060b568b85e0ddb0d8
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 17:57:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    newArray = []
+    
+    while i < array.length
+      newArray.push(yield(array[i]))
+      i += 1
+    end
 
-
+    newArray
+  else
+    return 'A block was not passed.'
+  end
+end
